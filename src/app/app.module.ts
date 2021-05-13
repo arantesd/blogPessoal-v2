403a37353f4b8a374f1caff613e19044530b89e6
unknown <danielam.arantes@gmail.com>
2021-05-13 12:26:12 -0300

    alterações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
